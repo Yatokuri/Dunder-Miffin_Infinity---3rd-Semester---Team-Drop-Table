17ac56d6d0aef00143045e2dbc89a95edd2d4a98
Naylin <naylinoohla@gmail.com>
2024-09-23 10:11:21 +0200

    Swagger Added to BE

---
+++ Obligatory-Assignment-1/server/api/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace api.Controllers;
+
+[ApiController]
+public class ProductController(ProductService service) : ControllerBase
+{
+    
+    [HttpGet]
+    [Route("api/product")]
+    public ActionResult GetAllProducts()
+    {
+        var products = service.GetAllProducts();
+        return Ok(products);
+    }
+}
--- Obligatory-Assignment-1/server/api/Program.cs
+++ Obligatory-Assignment-1/server/api/Program.cs
@@ -1,5 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
+builder.Services.AddCors();
+
+
+
+
 var app = builder.Build();
+app.UseOpenApi();
+app.UseSwaggerUi();
+
+app.UseCors( opts => {
+
+    opts.AllowAnyOrigin();
+
+    opts.AllowAnyMethod();
+
+    opts.AllowAnyHeader();
+
+});
+
 
 app.MapGet("/", () => "Hello World!");
 

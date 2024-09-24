c18d30e7871763fad45d35da49514ba03c29b77e
Naylin <naylinoohla@gmail.com>
2024-09-24 10:19:40 +0200

    Update Footer.tsx

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/Footer.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/Footer.tsx
@@ -2,16 +2,15 @@
 
 function Footer() {
     return (
-        <footer className="footer footer-center">
+        <footer style={{ position: 'absolute', bottom: 25, width: '99%' }} className="footer footer-center">
             <p>© 2023 - 2024</p>
-            <div style={{display: "flex", gap: "20px", justifyContent: "center"}}>
+            <div style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
                 <a href="https://www.linkedin.com/in/naylin-hla/">LinkedIn</a>
                 <a href="https://github.com/NaylinHla">Github</a>
                 <a href="https://chatgpt.com/">Sources</a>
             </div>
         </footer>
-
-    )
+    );
 }
 
 export default Footer;

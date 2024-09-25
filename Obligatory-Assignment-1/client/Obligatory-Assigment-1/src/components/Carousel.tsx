49a45788994a60af1fbe66286e26eae1c3a0f301
Naylin <naylinoohla@gmail.com>
2024-09-25 09:44:36 +0200

    Added Dynamic Carousel Pictures

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/Carousel.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/Carousel.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 interface CarouselProps {
     text: string;
+    imageSrc: string;
 }
 
-const Carousel: React.FC<CarouselProps> = ({ text }) => {
+const Carousel: React.FC<CarouselProps> = ({ text, imageSrc }) => {
     return (
         <div className="carousel-container flex flex-col items-center">
             <div className="carousel rounded-box w-64" style={{ marginRight: '2rem', marginLeft: '2rem' }}>
                 <div className="carousel-item w-full">
                     <img
-                        src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+                        src={imageSrc}
                         className="w-full"
                         alt="Tailwind CSS Carousel component"
                     />
--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/pages/Home.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/pages/Home.tsx
@@ -1,4 +1,9 @@
 import Carousel from "../components/Carousel.tsx";
+import CarouselPicture1 from "../components/Carousel/Carousel_Assets/CarouselPicture1.png";
+import CarouselPicture2 from "../components/Carousel/Carousel_Assets/CarouselPicture2.png";
+import CarouselPicture3 from "../components/Carousel/Carousel_Assets/CarouselPicture3.png";
+import CarouselPicture4 from "../components/Carousel/Carousel_Assets/CarouselPicture4.png";
+
 
 function Home() {
     return (
@@ -11,10 +16,10 @@ function Home() {
                 <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Go to shop</button>
             </div>
             <div className="flex justify-center items-center mt-12 gap-4">
-                <Carousel text={"Any color of the rainbow"} />
-                <Carousel text={"Made by Chinese children"} />
-                <Carousel text={"All profits go to space"} />
-                <Carousel text={"Improves productivity"} />
+                <Carousel text={"Any color of the rainbow"} imageSrc={CarouselPicture1} />
+                <Carousel text={"Made by Chinese children"} imageSrc={CarouselPicture2} />
+                <Carousel text={"All profits go to space"} imageSrc={CarouselPicture3} />
+                <Carousel text={"Improves productivity"} imageSrc={CarouselPicture4} />
             </div>
         </div>
     );

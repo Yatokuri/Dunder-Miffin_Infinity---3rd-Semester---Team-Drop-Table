4c88ee76c314ae25e54b2725b4a2720435c4f95e
Yatokuri <Tho.olesen@gmail.com>
2024-09-26 10:00:45 +0200

    Contact Us Page & Page Header

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/App.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/App.tsx
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar.tsx";
 import {Toaster} from "react-hot-toast";
 import Footer from "./components/Footer.tsx";
 import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
 
 function App() {
     return (
@@ -20,6 +21,7 @@ function App() {
                             <Route path={'/home'} element={<Home />} />
                             <Route path={'/*'} element={<NotFound />} />
                             <Route path={'/about'} element={<About />} />
+                            <Route path={'/contact-us'} element={<Contact />} />
                         </Routes>
                     </div>
                 </div>
---
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/ContactUs.tsx
@@ -0,0 +1,28 @@
+﻿function ContactUs() {
+    return (
+        <div className="hero bg-base-200 ">
+            <div className="hero-content flex-col lg:flex-row">
+                <img
+                    src="https://i.iheart.com/v3/re/new_assets/5f9acbf26e25de06d805edf7?ops=contain(1480,0)"
+                    className="max-w-sm rounded-lg shadow-2xl"/>
+                <div>
+                    <h1 className="text-5xl font-bold">Contact us</h1>
+                    <p className="py-6">
+                        If you have any difficulties or problems with either your product or the service you have
+                        received, dont hesitate to reach out and we
+                        shall do our utmost to relieve and fix whatever might have gone wrong to the best extent of our
+                        abilities.
+
+                        <br/>
+                        <br/>
+                        Email: Michael.Corporate@DunderMifflinInfinity.com
+                        <br/>
+                        Tel: (212) 555-6733
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ContactUs;

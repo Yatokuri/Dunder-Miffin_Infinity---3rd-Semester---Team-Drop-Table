4adc5715ee9e6fb72d45c337b33ad7fa021c0356
Yatokuri <Tho.olesen@gmail.com>
2024-09-26 10:32:09 +0200

    First Attempt at Profile Page

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/App.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/App.tsx
@@ -7,6 +7,7 @@ import {Toaster} from "react-hot-toast";
 import Footer from "./components/Footer.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
+import Profile from "./pages/Profile.tsx";
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
                             <Route path={'/*'} element={<NotFound />} />
                             <Route path={'/about'} element={<About />} />
                             <Route path={'/contact-us'} element={<Contact />} />
+                            <Route path={'/profile'} element={<Profile />} />
                         </Routes>
                     </div>
                 </div>
---
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/MyProfile.tsx
@@ -0,0 +1,38 @@
+﻿function MyProfile() {
+    return (
+        <>
+            <div className="flex mt-5">
+                <p> Possible profile picture here</p>
+            </div>
+            <div className="flex mt-5">
+                <p> Profile Name</p>
+            </div>
+            <div>
+                <p className="profileName"> PN</p>
+            </div>
+            <div className="flex mt-3">
+                <p> Email</p>
+            </div>
+            <div>
+                <p className="profileEmail"> EM</p>
+            </div>
+            <div className="flex mt-3">
+                <p> Address</p>
+            </div>
+            <div>
+                <p className="profileAddress"> AD</p>
+            </div>
+            <div className="flex mt-3">
+                <p> Phone Number</p>
+            </div>
+            <div>
+                <p className="profilePhoneNumber"> TLF</p>
+            </div>
+            <div className="flex mt-5">
+                <button> Change info</button>
+            </div>
+        </>
+    );
+}
+
+export default MyProfile;

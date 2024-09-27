c1fb2619bb5dbdf7a7b3e669af72eff475a94ca3
Naylin <naylinoohla@gmail.com>
2024-09-27 20:32:08 +0200

    suspect merge confligt resolution

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/App.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound.tsx";
 import NewOrderTest from "./pages/NewOrderTest.tsx";
 import NavBar from "./components/NavBar.tsx";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer.tsx";
 import About from "./pages/About.tsx";
 import Contact from "./pages/CustomService/Contact.tsx";
@@ -16,40 +16,30 @@ import WhistleBlowingPolicy from "./pages/CustomService/WhistleBlowingPolicy.tsx
 import FAQ from "./pages/CustomService/FAQ.tsx";
 import MyOrders from "./pages/MyOrders.tsx";
 import AllOrders from "./pages/AllOrders.tsx";
-import About from "./pages/About.tsx";
 
 function App() {
     return (
-        <div className="flex flex-col min-h-screen bg-gray-100"> {/* Flex container for full height */}
+        <div className="flex flex-col min-h-screen bg-gray-100">
             <BrowserRouter>
                 <Toaster />
                 <NavBar />
-                <div className="flex-grow m-2 pt-16"> {/* Makes the content area take up available space */}
+                <div className="flex-grow m-2 pt-16">
                     <Routes>
-                        <Route path={'/'} element={<Home />} />
-                        <Route path={'/home'} element={<Home />} />
-                        <Route path={'/ipsum'} element={<NewOrderTest />} />
-                        <Route path={'/customer-service'} element={<CustomService />} />
-                        <Route path={'/customer-service/contact-us'} element={<Contact />} />
-                        <Route path={'/customer-service/faq'} element={<FAQ />} />
-                        <Route path={'/customer-service/sales-and-delivery-conditions'} element={<SalesAndDeliveryConditions />} />
-                        <Route path={'/customer-service/gdpr-data-protection-policy'} element={<GDPRDataProtectionPolicy />} />
-                        <Route path={'/customer-service/cookie-policy'} element={<CookiePolicy />} />
-                        <Route path={'/customer-service/whistleblowing-policy'} element={<WhistleBlowingPolicy />} />
-                        <Route path={'/*'} element={<NotFound />} />
-                        <Route path={'/about'} element={<About />} />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/ipsum" element={<NewOrderTest />} />
+                        <Route path="/customer-service" element={<CustomService />} />
+                        <Route path="/customer-service/contact-us" element={<Contact />} />
+                        <Route path="/customer-service/faq" element={<FAQ />} />
+                        <Route path="/customer-service/sales-and-delivery-conditions" element={<SalesAndDeliveryConditions />} />
+                        <Route path="/customer-service/gdpr-data-protection-policy" element={<GDPRDataProtectionPolicy />} />
+                        <Route path="/customer-service/cookie-policy" element={<CookiePolicy />} />
+                        <Route path="/customer-service/whistleblowing-policy" element={<WhistleBlowingPolicy />} />
+                        <Route path="/*" element={<NotFound />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/myOrders" element={<MyOrders />} />
+                        <Route path="/allOrders" element={<AllOrders />} />
                     </Routes>
-                <div style={{ paddingTop: '4rem' }}> {/* Adjust according to NavBar height */}
-                    <div className="m-2"> {/* Global Margin */}
-                        <Routes>
-                            <Route path={'/'} element={<Home />} />
-                            <Route path={'/home'} element={<Home />} />
-                            <Route path={'/*'} element={<NotFound />} />
-                            <Route path={'/myOrders'} element={<MyOrders />} />
-                            <Route path={'/allOrders'} element={<AllOrders />} />
-                            <Route path={'/about'} element={<About />} />
-                        </Routes>
-                    </div>
                 </div>
                 <Footer />
             </BrowserRouter>

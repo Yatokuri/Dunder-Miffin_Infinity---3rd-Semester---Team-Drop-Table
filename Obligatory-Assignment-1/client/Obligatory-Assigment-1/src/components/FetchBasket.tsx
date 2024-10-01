6ef53a0c24cc960a145b982b9483eeea06cf1ee9
Yatokuri <Tho.olesen@gmail.com>
2024-10-01 11:28:04 +0200

    Semi working Store Page

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/FetchBasket.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/components/FetchBasket.tsx
@@ -1,9 +1,7 @@
 ﻿function FetchBasket() {
     return (
-        <div className="hero bg-base-200 ">
-            <div className="hero-content flex-col lg:flex-row">
-                
-            </div>
+        <div>
+            
         </div>
     );
 }
--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/pages/Shop/Shop.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/pages/Shop/Shop.tsx
@@ -1,5 +1,5 @@
 import {useAtom} from "jotai";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {productsAtom} from "../Basket.tsx";
 import {BasketAtom} from "../../atoms/BasketAtom.ts";
 import {Api} from "../../../Api.ts";
@@ -8,33 +8,40 @@ import {Api} from "../../../Api.ts";
 
 export const MyApi = new Api();
 
+
 function Shop() {
-    
-    
+
     const [products, setProducts] = useAtom(productsAtom);
     const [basket, setBasket] = useAtom(BasketAtom);
 
-    const ShopCard = (props) => {
+    const ShopCard = ({product}) => {
         return (
-            <div className="card">
-                <h2>{props.name}</h2>
-                <p>{props.price}</p>
-                <div>
-                    <button className="btn btn-sm bg-green-600 text-white hover:bg-green-700 w-5 h-5" onClick={addToBasket}>+</button>
-                    <p>{props.quantity}</p>
-                    <button className="btn btn-sm bg-red-600 text-white hover:bg-red-700 w-5 h-5" onClick={removeFromBasket}>-</button>
+            <div className="card card-compact bg-base-100 w-96 shadow-xl mr-5">
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes"/>
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{product.name}</h2>
+                    <p>Price {product.price}</p>
+                    <div className="card-actions justify-end font-bold">
+                        <button className="btn bg-green-500">+</button>
+                        <button className="btn bg-red-500">-</button>
+                    </div>
                 </div>
             </div>
         );
     }
-    const addToBasket = () => {
-        
-    }
     
+    const addToBasket = (productId: string) => {
+
+    }
+
     const removeFromBasket = () => {
-        
+
     }
-    
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -46,18 +53,18 @@ function Shop() {
         };
         fetchData();
     }, [setProducts]);
-    
+
     return (
-      <div>
-          <h1 className="text-3xl font-bold bg-center">Sales: Paper</h1>
-          <h1 className="text-2xl font-bold bg-center">For all your administrative needs</h1>
-          <div className="card-list">
-              {products.map((item, index) => (
-                  <ShopCard key={index} title={item.name} description={item.price} />
-              ))}
-          </div>
-      </div>
-  );
+        <div className="text-black">
+            <h1 className="text-3xl font-bold bg-center">Sales: Paper</h1>
+            <h1 className="text-2xl font-bold bg-center">For all your administrative needs</h1>
+            <div className="card-list flex flex-row w-full justify-around mt-10">
+                {products.filter(product => !product.discontinued).map((product, index) => (
+                    <ShopCard key={index} product={product}/>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Shop;

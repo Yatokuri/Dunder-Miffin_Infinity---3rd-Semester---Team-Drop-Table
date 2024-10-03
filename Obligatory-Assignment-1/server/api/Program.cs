845ab8a1b786cdf4d0488658d6eee93deb4655be
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-10-03 12:19:03 +0200

    testing the testing

--- Obligatory-Assignment-1/server/api/Program.cs
+++ Obligatory-Assignment-1/server/api/Program.cs
@@ -2,72 +2,75 @@ using System.Text.Json.Serialization;
 using dataAccess;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Load configuration from .env file
-var envFilePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "config.env");
-if (File.Exists(envFilePath))
+public class Program
 {
-    var envVariables = File.ReadAllLines(envFilePath);
-    foreach (var variable in envVariables)
+    public static void Main(string[] args)
     {
-        if (variable.Contains('='))
+        var builder = WebApplication.CreateBuilder(args);
+
+        var envFilePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "config.env");
+        if (File.Exists(envFilePath))
         {
-            var parts = variable.Split('=');
-            if (parts.Length == 2)
+            var envVariables = File.ReadAllLines(envFilePath);
+            foreach (var variable in envVariables)
             {
-                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+                if (variable.Contains('='))
+                {
+                    var parts = variable.Split('=');
+                    if (parts.Length == 2)
+                    {
+                        Environment.SetEnvironmentVariable(parts[0], parts[1]);
+                    }
+                }
             }
         }
-    }
-}
 
 // Construct the PostgreSQL connection string using environment variables
-var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
-var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+        var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+        var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
 
-var connectionString = $"Host=localhost;Database={database};Username={user};Password={password};";
+        var connectionString = $"Host=localhost;Database={database};Username={user};Password={password};";
 
 
-builder.Services.AddControllers();
-
+        builder.Services.AddControllers();
 
 
 // Register the DbContext with PostgreSQL using the constructed connection string
-builder.Services.AddDbContext<DMIContext>(options =>
-{
-    options.UseNpgsql(connectionString);
-    options.EnableSensitiveDataLogging();
-});
+        builder.Services.AddDbContext<DMIContext>(options =>
+        {
+            options.UseNpgsql(Environment.GetEnvironmentVariable("TestDB") ?? connectionString);
+            options.EnableSensitiveDataLogging();
+        });
 
-builder.Services.AddDbContext<DMIContext>(opt => opt.UseInMemoryDatabase("DMI"));
+        builder.Services.AddDbContext<DMIContext>(opt => opt.UseInMemoryDatabase("DMI"));
 
-builder.Services.AddOpenApiDocument(configure =>
-{
-    configure.Title = "Dunder Mifflin Infinity"; // Set your API title
-    configure.Description = "Try and test"; // Set your API description
-    configure.Version = "v1"; //test of my branch
-});
+        builder.Services.AddOpenApiDocument(configure =>
+        {
+            configure.Title = "Dunder Mifflin Infinity"; // Set your API title
+            configure.Description = "Try and test"; // Set your API description
+            configure.Version = "v1"; //test of my branch
+        });
 
-builder.Services.AddCors();
-
-
-var app = builder.Build();
-app.UseOpenApi();
-app.UseSwaggerUi();
-
-app.MapControllers();
-
-app.UseCors( opts => {
-
-    opts.AllowAnyOrigin();
-
-    opts.AllowAnyMethod();
-
-    opts.AllowAnyHeader();
-
-});
+        builder.Services.AddCors();
 
 
-app.Run();
+        var app = builder.Build();
+        app.UseOpenApi();
+        app.UseSwaggerUi();
+
+        app.MapControllers();
+
+        app.UseCors(opts =>
+        {
+            opts.AllowAnyOrigin();
+
+            opts.AllowAnyMethod();
+
+            opts.AllowAnyHeader();
+        });
+
+
+        app.Run();
+    }
+}

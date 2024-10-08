0c4e6c08e381ab73b1f9e3b31afefb8968070d16
Yatokuri <Tho.olesen@gmail.com>
2024-10-08 10:12:45 +0200

    Fixes so Github doesnt complain anymore

--- Obligatory-Assignment-1/server/dataAccess/Models/Product.cs
+++ Obligatory-Assignment-1/server/dataAccess/Models/Product.cs
@@ -3,7 +3,7 @@
 public class Product
 {
     public int Id { get; set; }
-    public String Name { get; set; }
+    public String Name { get; set; } = null!;
     public bool Discontinued { get; set; }
     public int Stock { get; set; }
     public float Price { get; set; }

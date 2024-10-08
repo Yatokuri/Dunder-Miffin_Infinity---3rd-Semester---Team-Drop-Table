97fce5fd4e2c53d1400d657b3fdff7992f9925bf
Naylin <naylinoohla@gmail.com>
2024-10-08 18:00:11 +0200

    ui changes

--- Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/pages/Admin/Admin.tsx
+++ Obligatory-Assignment-1/client/Obligatory-Assigment-1/src/pages/Admin/Admin.tsx
@@ -18,24 +18,26 @@ function Admin() {
     return (
         <div className="flex flex-col items-center">
             <div>
-                <h1 className="text-black text-6xl">Admin Page</h1>
+                <h1 className="text-black text-6xl mt-16">Admin Page</h1>
             </div>
             <div className="flex flex-row w-full justify-around mt-10">
                 <ProductTable />
                 <PropertyTable />
             </div>
-            <button
-                className="btn btn-md lg:btn-lg bg-blue-600 text-white hover:bg-blue-700 transition duration-200 mt-10"
-                onClick={openProductModal}
-            >
-                Add Product
-            </button>
-            <button
-                className="btn btn-md lg:btn-lg bg-green-600 text-white hover:bg-green-700 transition duration-200 mt-10"
-                onClick={openPropertyModal}
-            >
-                Add Property
-            </button>
+            <div className="flex flex-row space-x-4 mt-10">
+                <button
+                    className="btn btn-md lg:btn-lg bg-blue-600 text-white hover:bg-blue-700 transition duration-200"
+                    onClick={openProductModal}
+                >
+                    Add Product
+                </button>
+                <button
+                    className="btn btn-md lg:btn-lg bg-green-600 text-white hover:bg-green-700 transition duration-200"
+                    onClick={openPropertyModal}
+                >
+                    Add Property
+                </button>
+            </div>
             <Modal isOpen={isProductModalOpen} onClose={closeProductModal}>
                 <AddProduct closeModal={closeProductModal} />
             </Modal>

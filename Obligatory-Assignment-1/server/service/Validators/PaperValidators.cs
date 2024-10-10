288a42eecadb07387f337f35e2f6834016aed527
Yatokuri <Tho.olesen@gmail.com>
2024-10-10 09:32:21 +0200

    Initial Validator - Not Working but just to publish branch

--- Obligatory-Assignment-1/server/service/Validators/PaperValidators.cs
+++ Obligatory-Assignment-1/server/service/Validators/PaperValidators.cs
@@ -1,4 +1,6 @@
+using dataAccess.Models;
 using FluentValidation;
+using service.Request;
 
 namespace Service.Validators
 {
@@ -7,6 +9,35 @@ namespace Service.Validators
         public PaperValidator()
         {
 
+        }
+    }
+    public class CreatePaperValidator : AbstractValidator<CreatePaperDto>
+    {
+        public CreatePaperValidator()
+        {
+            RuleFor(paper => paper.name)
+                .NotEmpty().WithMessage("Name cannot be empty.");
+
+            RuleFor(paper => paper.price)
+                .NotEmpty().WithMessage("Price cannot be empty.")
+                .GreaterThanOrEqualTo(1).WithMessage("Order Date cannot be in the future.");
+
+            RuleFor(paper => paper.stock)
+                .NotEmpty().WithMessage("Stock cannot be empty")
+                .GreaterThanOrEqualTo(0).WithMessage("Stock must be greater than or equal to 0.");
+        }
+    }
+
+    public class UpdatePaperValidator : AbstractValidator<EditPaperDto>
+    {
+        public UpdatePaperValidator()
+        {
+            RuleFor(order => order.Status)
+                .NotEmpty().WithMessage("Status cannot be empty.");
+
+            /*  RuleFor(order => order.DeliveryDate)
+                  .NotEmpty().WithMessage("Delivery Date cannot be empty.")
+                  .GreaterThan(DateTime.UtcNow.Date).WithMessage("Delivery Date must be in the future."); */
         }
     }
 }
